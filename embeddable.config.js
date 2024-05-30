de48f8f8b5978b5ff881e3fa74c63411359c4d4c
Gonzalo Rubio <gonzalo.external@getcakewalk.io>
2024-05-30 15:59:18 +0200

    Initial commit

--- embeddable.config.js
+++ embeddable.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from '@embeddable.com/sdk-core';
 import react from '@embeddable.com/sdk-react';
 
 export default defineConfig({
-  plugins: [react]
-
-  // previewBaseUrl: 'https://app.dev.embeddable.com',
-  // pushBaseUrl: 'https://api.dev.embeddable.com',
-  // audienceUrl: 'https://api.dev.embeddable.com/',
-  // authDomain: 'embeddable-dev.eu.auth0.com',
-  // authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG',
+  plugins: [react],
+  previewBaseUrl: 'https://app.eu.embeddable.com',
+  pushBaseUrl: 'https://app-api.eu.embeddable.com',
+  audienceUrl: 'https://auth.eu.embeddable.com',
+  authDomain: 'auth.eu.embeddable.com',
+  authClientId: process.env.AUTH_CLIENT_ID,
 });
