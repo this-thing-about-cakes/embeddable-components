872ab0de9b8ef0e7121d5a3df098abaff0a591e1
Gonzalo R. <gonzalo.rubio@getcakewalk.io>
2024-06-13 21:16:35 +0200

    V1

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,5 +1,5 @@
 export const EMB_FONT =
-  '-apple-system, "system-ui", "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+  'Roboto';
 
 export const FONTS = {
   // 'Open Sans': 'OpenSans.ttf'
@@ -13,18 +13,30 @@ export const REGULAR_FONT_SIZE = '14px'
 
 //general chart colors
 export const COLORS = [
-  '#2859C5', 
-  '#F58D02',
-  '#964FD2',
-  '#FF6B6C',
-  '#B8B8D1',
-  '#FFC145',
-  '#4473D9',
-  '#FDA32B',
-  '#AF79DD',
-  '#FF9E9F',
-  '#D7D7E5',
-  '#FFD37A'
+  // '#2859C5',
+  // '#F58D02',
+  // '#964FD2',
+  // '#FF6B6C',
+  // '#B8B8D1',
+  // '#FFC145',
+  // '#4473D9',
+  // '#FDA32B',
+  // '#AF79DD',
+  // '#FF9E9F',
+  // '#D7D7E5',
+  // '#FFD37A'
+  '#CEA8F6',
+  '#F5BC2D',
+  '#54A17C',
+  '#678CB8',
+  '#EE7D5F',
+  '#E95757',
+  '#6B7280',
+  '#895DB7',
+  '#B88402',
+  '#E0E0E0',
+  '#F9F9FB',
+  '#CADBEF',
 ];
 
 export const DATE_DISPLAY_FORMATS = {
