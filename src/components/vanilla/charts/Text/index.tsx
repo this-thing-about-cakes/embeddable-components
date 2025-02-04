d69a86f9b1f3c1b3421d869187bdea7809672786
Gonzalo R. <gonzalo.rubio@getcakewalk.io>
2025-02-04 10:09:55 +0100

    Last changes

--- src/components/vanilla/charts/Text/index.tsx
+++ src/components/vanilla/charts/Text/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Title from '../../Title';
 import Description from '../../Description';
+import { fontStyle } from 'html2canvas/dist/types/css/property-descriptors/font-style';
+import { fontWeight } from 'html2canvas/dist/types/css/property-descriptors/font-weight';
 
 type Props = {
   title?: string;
@@ -12,10 +14,22 @@ type Props = {
 export default (props: Props) => {
   const { title, body, titleFontSize, bodyFontSize  } = props;
 
+  const titleStyle = {
+    fontSize: titleFontSize ? `${titleFontSize}px` : undefined,
+    lineHeight: titleFontSize ? '1.2em' : undefined
+  }
+
+  const bodyStyle = {
+    fontSize: '1.5rem',
+    lineHeight: bodyFontSize ? '1.2em' : undefined,
+    fontStyle: 'normal',
+    fontWeight: 500
+  }
+
   return (
     <div className='cakewalk-title'>
-      <Title title={title} />
-      <Description description={body}/>
+      <Title title={title} style={titleStyle}/>
+      <Description description={body} style={bodyStyle}/>
     </div>
   );
 };
